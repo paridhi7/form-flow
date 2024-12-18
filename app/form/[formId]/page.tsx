2c6bb3ee126d86398808381f67469bcd225bf35b
paridhi7 <theparidhi0@gmail.com>
2024-12-19 01:38:29 +0530

    fix hydration issue

--- app/form/[formId]/page.tsx
+++ app/form/[formId]/page.tsx
@@ -1,7 +1,11 @@
 'use client'
-import FormBuilder from "@/app/components/form-builder/FormBuilder"
 
-export default function FormBuilderPage({ params }: { params: { formId: string } }) {
+import FormBuilder from "@/app/components/form-builder/FormBuilder";
+import { useParams } from "next/navigation";
+
+export default function FormBuilderPage() {
+  const params = useParams<{ formId: string }>()
+  
   return (
     <div className="h-screen flex flex-col">
       <FormBuilder formId={params.formId} />
