941eba1410299b4121755441670b6d2ef0c3a594
paridhi7 <theparidhi0@gmail.com>
2024-12-30 17:41:16 +0530

    Add session strategy in options, redirect logic in root path

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -28,6 +28,9 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: '/auth/signin',
   },
+  session: {
+    strategy: "jwt",
+  },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === 'development',
 }
